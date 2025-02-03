e2f6d4f1235e5cfe021e21203694038cf0f6a95e
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-02-03 21:10:12 +0200

    add the hello handler function and necessary error handling

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+}
 
 func main() {
 	fmt.Println("Hello World!")
