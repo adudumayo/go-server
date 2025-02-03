283b67778d39f8092d81c18be7776c66b6614128
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2025-02-03 22:16:47 +0200

    Add function for handling the form submit

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -10,8 +11,37 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "404 not found", http.StatusNotFound)
 		return
 	}
+
+	if r.Method != "GET" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "hello")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() error %v", err)
+		return
+	}
+
+	fmt.Fprintf(w, "Post Request Successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w, "The name is %s\n", name)
+	fmt.Fprintf(w, "The address is %s\n", address)
 }
 
 func main() {
-	fmt.Println("Hello World!")
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("Starting the server at port 8080")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
